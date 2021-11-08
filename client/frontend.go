c71168426e1c32310adec075ee05671c71724827
yoRyuuuuu <sasoseooo@gmail.com>
2021-11-08 13:31:10 +0900

    変数名、関数名を変更

--- client/frontend.go
+++ client/frontend.go
@@ -13,10 +13,9 @@ const refreshInterval = 16 * time.Millisecond
 
 type View struct {
 	app           *tview.Application
-	flex          *tview.Flex
-	rightFlex     *tview.Flex
-	leftFlex      *tview.Flex
-	playerText    map[string]*tview.TextView
+	mainView      *tview.Flex
+	playerView    *tview.Flex
+	playerStatus  map[string]*tview.TextView
 	drawCallbacks []func()
 	*Game
 }
@@ -32,22 +31,22 @@ func (v *View) refresh() {
 	}
 }
 
-func setupWord(view *View) {
+func (v *View) setupWordView() {
 	word := tview.NewTextView()
 	word.SetTitle("Word").
 		SetBorder(true)
 
 	callback := func() {
-		w := view.word
+		w := v.word
 		word.SetText(w)
 	}
 
-	view.drawCallbacks = append(view.drawCallbacks, callback)
+	v.drawCallbacks = append(v.drawCallbacks, callback)
 
-	view.leftFlex.AddItem(word, 3, 0, false)
+	v.mainView.AddItem(word, 3, 0, false)
 }
 
-func setupInput(view *View) {
+func (v *View) setupInputField() {
 	input := tview.NewInputField()
 	input.SetLabel("input: ")
 
@@ -57,8 +56,8 @@ func setupInput(view *View) {
 	input.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
 		case tcell.KeyEnter:
-			if view.checkAnswer(input.GetText()) {
-				view.actionChannel <- AnswerAction{
+			if v.checkAnswer(input.GetText()) {
+				v.actionChannel <- AnswerAction{
 					text: input.GetText(),
 				}
 
@@ -70,46 +69,47 @@ func setupInput(view *View) {
 		return event
 	})
 
-	view.leftFlex.AddItem(input, 3, 0, true)
+	v.mainView.AddItem(input, 3, 0, true)
 }
 
-func setupStatus(view *View) {
+func (v *View) setupLogger() {
 	status := tview.NewTextView()
 	status.SetTitle("Log").
 		SetBorder(true)
 
 	callback := func() {
-		status.SetText(view.log)
+		status.SetText(v.log)
 	}
 
-	view.drawCallbacks = append(view.drawCallbacks, callback)
-	view.leftFlex.AddItem(status, 0, 1, false)
+	v.drawCallbacks = append(v.drawCallbacks, callback)
+	v.mainView.AddItem(status, 0, 1, false)
 }
 
-func setupPlayer(view *View) {
+func (v *View) setupPlayerView() {
 	text := tview.NewTextView()
 	text.SetTitle(fmt.Sprintf("YOU")).
 		SetBorder(true)
 	text.SetText(fmt.Sprintf("score: 0"))
-	view.rightFlex.AddItem(text, 3, 0, false)
-	view.playerText[view.id] = text
+	v.playerView.AddItem(text, 3, 0, false)
+	v.playerStatus[v.id] = text
+	v.drawCallbacks = append(v.drawCallbacks, v.drawPlayerView)
+}
 
-	callback := func() {
-		select {
-		case player := <-view.playerChannel:
-			text := tview.NewTextView()
-			text.SetTitle(player.name).
-				SetBorder(true)
-			text.SetText(fmt.Sprintf("score: 0"))
-			view.rightFlex.AddItem(text, 3, 0, false)
-			view.playerText[player.id] = text
-		case damage := <-view.damageChannel:
-			view.playerText[damage.id].SetText(fmt.Sprintf("score: %v", damage.damage))
-		default:
-		}
+func (v *View) drawPlayerView() {
+	select {
+	case player := <-v.playerChannel:
+		text := tview.NewTextView()
+		text.SetTitle(player.name).
+			SetBorder(true)
+		text.SetText(fmt.Sprintf("score: 0"))
+		v.playerView.AddItem(text, 3, 0, false)
+		v.playerStatus[player.id] = text
+	case damage := <-v.damageChannel:
+		v.playerStatus[damage.id].SetText(fmt.Sprintf("score: %v", damage.damage))
+	default:
 	}
 
-	view.drawCallbacks = append(view.drawCallbacks, callback)
+	return
 }
 
 func NewView(game *Game) *View {
@@ -117,34 +117,32 @@ func NewView(game *Game) *View {
 	app := tview.NewApplication()
 
 	flex := tview.NewFlex()
-	flex.SetBackgroundColor(tcell.ColorBlack)
-	leftFlex := tview.NewFlex().SetDirection(tview.FlexRow)
-	flex.AddItem(leftFlex, 0, 2, true)
-	rightFlex := tview.NewFlex().SetDirection(tview.FlexRow)
-	flex.AddItem(rightFlex, 0, 1, false)
-
+	mainView := tview.NewFlex().SetDirection(tview.FlexRow)
+	flex.AddItem(mainView, 0, 2, true)
+	playerView := tview.NewFlex().SetDirection(tview.FlexRow)
+	flex.AddItem(playerView, 0, 1, false)
 	view := &View{
 		app:           app,
-		leftFlex:      leftFlex,
-		rightFlex:     rightFlex,
-		playerText:    map[string]*tview.TextView{},
+		mainView:      mainView,
+		playerView:    playerView,
+		playerStatus:  map[string]*tview.TextView{},
 		drawCallbacks: []func(){},
 		Game:          game,
 	}
 
-	setupWord(view)
-	setupInput(view)
-	setupStatus(view)
-	setupPlayer(view)
+	view.setupWordView()
+	view.setupInputField()
+	view.setupLogger()
+	view.setupPlayerView()
 
-	view.flex = flex
+	view.app.SetRoot(flex, true)
 	return view
 }
 
 func (v *View) Start() {
 	go v.refresh()
 
-	if err := v.app.SetRoot(v.flex, true).Run(); err != nil {
+	if err := v.app.Run(); err != nil {
 		panic(err)
 	}
 }
