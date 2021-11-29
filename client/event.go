b96e0386a8279956a6f50251cf8a6fdd806be9aa
yoRyuuuuu <sasoseooo@gmail.com>
2021-11-29 21:34:16 +0900

    コメントを追加

--- client/event.go
+++ client/event.go
@@ -2,31 +2,39 @@ package client
 
 type Event interface{}
 
+// ゲーム終了Event
 type FinishEvent struct {
 	Event
 	Winner string
 }
 
+// お題Event
 type QuestionEvent struct {
 	Event
 	Text string
 }
 
+// ゲーム開始Event
 type StartEvent struct {
 	Event
 }
 
+// ダメージEvent
 type DamageEvent struct {
 	Event
-	// 攻撃を受けるPlayerのID
+	// 攻撃を受けるPlayerID
 	ID string
 	// ダメージの数値
 	Damage int
 }
 
+// プレイヤー参加Event
 type JoinEvent struct {
 	Event
-	ID     string
-	Name   string
+	// PlayerID
+	ID string
+	// Playerの名前
+	Name string
+	// 初期体力
 	Health int
 }
